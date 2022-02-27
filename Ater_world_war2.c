5fb2285ddd21a2ed8e0e53b8a06cf7d20598a9dd
sushantkhadka20xx <itsmesushant05@gmail.com>
2022-02-27 15:22:02 +0545

    Added about world war 2 power

--- Ater_world_war2.c
+++ Ater_world_war2.c
@@ -2,20 +2,20 @@
 
 int main()
 {
-
-    struct Major_Axis_power
+    //--At the end of World war, Whole world is divided into two parts---
+    struct Major_Axis_power  //---Axis power by German,Italy and Japan--
     {
         char German[20];
         char Italian[20];
         char Japanese[20];
     };
-    struct Kingdom
+    struct Kingdom //--axis power kingdoms---
     {
         char German_[20];
         char Italian_[20];
         char Japanese_[20];
     };
-    struct Major_Allies_Power
+    struct Major_Allies_Power //--Allies power by uk, soviet union and US--
     {
         char Uk[20];
         char Sovit[20];
@@ -31,15 +31,16 @@ int main()
     {"German","Italian","Japanese"},
     {"German Reich","Kingdom of Itsly","Empire of Japan"}};
     
-    printf("\nA. Major_Axis_Power Country:\n");
+    //----Printing all the value of Above into the order---
+    printf("\nA. Major_Axis_Power Country:\n"); //--Axis power-
     printf("1. %s\n",world.Axis.German);
     printf("2. %s\n",world.Axis.Italian);
     printf("3. %s\n\n",world.Axis.Japanese);
-    printf("B. Major_Axis_Power Country's Kingdom:\n");
+    printf("B. Major_Axis_Power Country's Kingdom:\n"); //--Axix power kingdoms--
     printf("1. %s\n",world.King.German_);
     printf("2. %s\n",world.King.Italian_);
     printf("3. %s\n",world.King.Japanese_);
-    printf("\nC. Major_Allies_Power:\n");
+    printf("\nC. Major_Allies_Power:\n"); //--Allies power--
     printf("1. %s\n",world.Allis.Uk);
     printf("2. %s\n",world.Allis.Sovit);
     printf("3. %s\n",world.Allis.US);
--- notes.c
+++ notes.c
@@ -1495,3 +1495,53 @@ int main()
     return 0;
 }
 //______________________________________________________________
+
+#include <stdio.h>
+
+int main()
+{
+    //--At the end of World war, Whole world is divided into two parts---
+    struct Major_Axis_power  //---Axis power by German,Italy and Japan--
+    {
+        char German[20];
+        char Italian[20];
+        char Japanese[20];
+    };
+    struct Kingdom //--axis power kingdoms---
+    {
+        char German_[20];
+        char Italian_[20];
+        char Japanese_[20];
+    };
+    struct Major_Allies_Power //--Allies power by uk, soviet union and US--
+    {
+        char Uk[20];
+        char Sovit[20];
+        char US[20];
+    };
+    struct country
+    {
+        struct Major_Allies_Power Allis;
+        struct Major_Axis_power Axis;
+        struct Kingdom King;
+    };
+    struct country world = {{"United Kingdom","Soviet Union","United states"},
+    {"German","Italian","Japanese"},
+    {"German Reich","Kingdom of Itsly","Empire of Japan"}};
+    
+    //----Printing all the value of Above into the order---
+    printf("\nA. Major_Axis_Power Country:\n"); //--Axis power-
+    printf("1. %s\n",world.Axis.German);
+    printf("2. %s\n",world.Axis.Italian);
+    printf("3. %s\n\n",world.Axis.Japanese);
+    printf("B. Major_Axis_Power Country's Kingdom:\n"); //--Axix power kingdoms--
+    printf("1. %s\n",world.King.German_);
+    printf("2. %s\n",world.King.Italian_);
+    printf("3. %s\n",world.King.Japanese_);
+    printf("\nC. Major_Allies_Power:\n"); //--Allies power--
+    printf("1. %s\n",world.Allis.Uk);
+    printf("2. %s\n",world.Allis.Sovit);
+    printf("3. %s\n",world.Allis.US);
+
+    return 0;
+}
